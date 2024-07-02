62935ad183d84083b0c47e87e9f4284cfe04778a
Imad Asserne <imad.asserne@gmail.com>
2024-07-02 12:33:06 +0100

    Delete Product component & add Profile

--- src/app/customer/customer.module.ts
+++ src/app/customer/customer.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { CustomerRoutingModule } from './customer-routing.module';
 import { CustomerComponent } from './customer.component';
 import { CustomerDashboardComponent } from './component/customer-dashboard/customer-dashboard.component';
+import { ProfileComponent } from './component/profile/profile.component';
 
 
 @NgModule({
   declarations: [
     CustomerComponent,
-    CustomerDashboardComponent
+    CustomerDashboardComponent,
+    ProfileComponent
   ],
   imports: [
     CommonModule,
