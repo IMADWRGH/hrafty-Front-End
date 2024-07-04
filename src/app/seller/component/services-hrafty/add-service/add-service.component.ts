ce719e1070184bee655ec0baa35b48b79b867e13
Imad Asserne <imad.asserne@gmail.com>
2024-07-04 12:14:25 +0100

    fix error **SellerID

--- src/app/seller/component/services-hrafty/add-service/add-service.component.ts
+++ src/app/seller/component/services-hrafty/add-service/add-service.component.ts
@@ -1,7 +1,9 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { Seller } from 'src/app/models/Seller.model';
 import { Service } from 'src/app/models/Service.model';
+import { SellerService } from 'src/app/seller/services/seller.service';
 import { ServiceService } from 'src/app/seller/services/service.service';
 import { UserStorageService } from 'src/app/shared/services/storage/user-storage.service';
 
@@ -10,17 +12,22 @@ import { UserStorageService } from 'src/app/shared/services/storage/user-storage
   templateUrl: './add-service.component.html',
   styleUrls: ['./add-service.component.css']
 })
-export class AddServiceComponent {
-  user = UserStorageService.getUser();
-  sellerId: number = UserStorageService.getUserId();
+export class AddServiceComponent implements OnInit {
+  userId: number = UserStorageService.getUserId();
   inputdata: any;
   newService: Service;
   editService: Service;
   isEditMode: boolean = false;
   serviceForm?: FormGroup;
+  sellerId: number;
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any, private service: ServiceService, private fb: FormBuilder, private ref: MatDialogRef<AddServiceComponent>, private serviceImpl: ServiceService) {
-  }
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private service: ServiceService,
+    private fb: FormBuilder,
+    private ref: MatDialogRef<AddServiceComponent>,
+    private sellerService: SellerService
+  ) { }
 
   ngOnInit(): void {
     this.inputdata = this.data;
@@ -40,9 +47,26 @@ export class AddServiceComponent {
       this.isEditMode = true;
       this.serviceForm.patchValue(this.inputdata.service);
     }
+    this.getSellerId();
+  }
+
+  getSellerId(): void {
+    this.sellerService.getSellerData(this.userId).subscribe({
+      next: (data: Seller) => {
+        this.sellerId = data.id;
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    });
   }
 
   onSubmit(): void {
+    if (!this.sellerId) {
+      console.log('Seller ID not available yet.');
+      return;
+    }
+
     const serviceData = {
       ...this.serviceForm.value,
       sellerId: this.sellerId
@@ -52,12 +76,20 @@ export class AddServiceComponent {
       this.service.updateService(serviceData).subscribe({
         next: (res) => {
           this.closepopup();
+          console.log(res);
+        },
+        error: (err) => {
+          console.log(err);
         }
       });
     } else {
       this.service.addService(serviceData).subscribe({
         next: (res) => {
+          console.log(res);
           this.closepopup();
+        },
+        error: (err) => {
+          console.log(err);
         }
       });
     }
@@ -68,7 +100,7 @@ export class AddServiceComponent {
   }
 
   setpopudate(id: number) {
-    this.serviceImpl.getService(id).subscribe({
+    this.service.getService(id).subscribe({
       next: (res) => {
         this.editService = res;
         this.serviceForm.setValue({
@@ -80,6 +112,9 @@ export class AddServiceComponent {
           type: this.editService.type,
           status: this.editService.status
         });
+      },
+      error: (err) => {
+        console.log(err);
       }
     });
   }
--- src/app/seller/component/services-hrafty/list-services/list-services.component.ts
+++ src/app/seller/component/services-hrafty/list-services/list-services.component.ts
@@ -1,66 +1,96 @@
-import { Component, ViewChild } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Service } from 'src/app/models/Service.model';
-import { ServiceService } from 'src/app/seller/services/service.service';
-import { UserStorageService } from 'src/app/shared/services/storage/user-storage.service';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatDialog } from '@angular/material/dialog';
+import { Service } from 'src/app/models/Service.model';
+import { ServiceService } from 'src/app/seller/services/service.service';
+import { SellerService } from 'src/app/seller/services/seller.service';
+import { UserStorageService } from 'src/app/shared/services/storage/user-storage.service';
 import { AddServiceComponent } from '../add-service/add-service.component';
+import { Seller } from 'src/app/models/Seller.model';
+
 @Component({
   selector: 'app-list-services',
   templateUrl: './list-services.component.html',
   styleUrls: ['./list-services.component.css']
 })
-export class ListServicesComponent {
+export class ListServicesComponent implements OnInit {
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
   services: Service[] = [];
-  data = UserStorageService.getUser();
-  sellerId: number = UserStorageService.getUserId();
+  userId = UserStorageService.getUserId();
+  sellerId: number;
   datasource: MatTableDataSource<Service>;
   columns: string[] = ['Name', 'Description', 'Price', 'Type', 'Status', 'Action'];
-  constructor(private serviceService: ServiceService, private dialog: MatDialog) {
-    this.loadServices();
+
+  constructor(
+    private serviceService: ServiceService,
+    private dialog: MatDialog,
+    private sellerService: SellerService
+  ) { }
+
+  ngOnInit(): void {
+    this.getSellerId();
   }
+
+  getSellerId(): void {
+    this.sellerService.getSellerData(this.userId).subscribe({
+      next: (data: Seller) => {
+        this.sellerId = data.id;
+        this.loadServices();
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    });
+  }
+
   loadServices() {
-    this.serviceService.getServices(this.sellerId).subscribe((reponse: Service[]) => {
-      this.services = reponse;
-      this.datasource = new MatTableDataSource<Service>(this.services);
-      this.datasource.paginator = this.paginator;
-      this.datasource.sort = this.sort;
+    if (!this.sellerId) {
+      console.error('Seller ID is not available yet.');
+      return;
+    }
+
+    this.serviceService.getServices(this.sellerId).subscribe({
+      next: (response: Service[]) => {
+        this.services = response;
+        this.datasource = new MatTableDataSource<Service>(this.services);
+        this.datasource.paginator = this.paginator;
+        this.datasource.sort = this.sort;
+      },
+      error: (err) => {
+        console.log(err);
+      }
     });
   }
 
   detailService(id: number) {
-
   }
 
   deleteService(id: number) {
     this.serviceService.deleteService(id).subscribe({
       next: (res) => {
         console.log(res);
+        this.loadServices();
       },
       error: (err) => {
         console.log(err);
       }
     });
-    this.loadServices();
   }
-
 
   editService(id: number) {
     this.openpopup(id, "Edit Service");
     console.log(id);
-
   }
 
   addService() {
-    this.openpopup(0, "Add new  Service");
+    this.openpopup(0, "Add new Service");
   }
+
   openpopup(id: number, title: string) {
-    var _add = this.dialog.open(AddServiceComponent, {
+    const dialogRef = this.dialog.open(AddServiceComponent, {
       exitAnimationDuration: '1000ms',
       width: '80%',
       height: '80%',
@@ -69,14 +99,12 @@ export class ListServicesComponent {
         id: id
       },
     });
-    _add.afterClosed().subscribe((result) => {
+
+    dialogRef.afterClosed().subscribe((result) => {
       this.loadServices();
     });
   }
+
   Filterchange($event: KeyboardEvent) {
-    throw new Error('Method not implemented.');
   }
-
-
-
 }
