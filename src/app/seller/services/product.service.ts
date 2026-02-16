1213beca54986807bef707c4c078b3cc4b0174c5
imad ASSERNE <imad.asserne@gmail.com>
2026-02-16 23:45:25 +0100

    refactor(product-service): use environment variable for API URL

--- src/app/seller/services/product.service.ts
+++ src/app/seller/services/product.service.ts
@@ -1,17 +1,17 @@
-import { Portal } from '@angular/cdk/portal';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { AppComponent } from 'src/app/app.component';
 import { Product } from 'src/app/models/Product.model';
-const API_URL = "http://localhost:8080/api/v1/product/";
+import { environment } from 'src/environments/environment';
+
+const API_URL = `${environment.apiBaseUrl}/product/`;
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) { }
 
   addProduct(product: Product, files: File[]): Observable<Product> {
     const formData: FormData = new FormData();
@@ -24,26 +24,26 @@ export class ProductService {
     return this.http.post<Product>(`${API_URL}add`, formData, { headers });
   }
 
-  updateProduct(id:number,product: Product): Observable<Product> {
+  updateProduct(id: number, product: Product): Observable<Product> {
     const formData: FormData = new FormData();
     formData.append('product', JSON.stringify(product));
     // files.forEach(file => formData.append('files', file, file.name));
     const headers = new HttpHeaders({
       'Accept': 'application/json'
     });
-    return this.http.put<Product>(`${API_URL}update/`+id, formData, { headers });
+    return this.http.put<Product>(`${API_URL}update/` + id, formData, { headers });
   }
 
   // addProduct(product:Product):Observable<Product>{
   //   return this.http.post<Product>(`${API_URL}add`,product);
   // }
 
-  deleteProduct(id:number){
-    return this.http.delete(`${API_URL}delete/`+id);
+  deleteProduct(id: number) {
+    return this.http.delete(`${API_URL}delete/` + id);
   }
 
-  getProducts(id:number):Observable<Product[]>{
-    return this.http.get<Product[]>(`${API_URL}getAll/`+id);
+  getProducts(id: number): Observable<Product[]> {
+    return this.http.get<Product[]>(`${API_URL}getAll/` + id);
   }
   getAllProducts(): Observable<Product[]> {
     return this.http.get<Product[]>(`${API_URL}getAll/`);
@@ -53,7 +53,7 @@ export class ProductService {
   //   return this.http.get<Product>(`${API_URL}update/`+id);
   // }
 
-  getProduct(id:number):Observable<Product>{
-    return this.http.get<Product>(`${API_URL}`+id);
+  getProduct(id: number): Observable<Product> {
+    return this.http.get<Product>(`${API_URL}` + id);
   }
 }
