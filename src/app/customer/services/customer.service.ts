ecb695f194ebfd77f92a4cee2c46c1db97483c16
imad ASSERNE <imad.asserne@gmail.com>
2026-02-16 23:43:44 +0100

    refactor(customer-service): use environment variable for API URL

--- src/app/customer/services/customer.service.ts
+++ src/app/customer/services/customer.service.ts
@@ -2,13 +2,15 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Customer } from 'src/app/models/Customer.model';
-const API_URL = "http://localhost:8080/api/v1/customer/";
+import { environment } from 'src/environments/environment';
+
+const API_URL = `${environment.apiBaseUrl}/customer/`;
 @Injectable({
   providedIn: 'root'
 })
 export class CustomerService {
 
-  constructor(private http :HttpClient) { }
+  constructor(private http: HttpClient) { }
 
   getCustomerData(id: number): Observable<Customer> {
     return this.http.get<Customer>(API_URL + id);
